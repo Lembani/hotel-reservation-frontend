5f1e5f69b0170352e85d29148fce322cfdadc237
Chia Carlyle <chiacarlyle@gmail.com>
2022-09-15 23:48:58 +0100

    Create basic design for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/fontawesome-free-solid';
 
 const Home = () => (
-  <div>Home</div>
+  <div className="home">
+    <nav>
+      <FontAwesomeIcon icon="fa-solid fa-bars" />
+      <FontAwesomeIcon icon={faSearch} className="search" />
+    </nav>
+    <main>
+      <strong>Welcome to...</strong>
+      <h1 className="logo">HoVe</h1>
+      <p>Acquire what you desire, reserve what you really deserve!!</p>
+    </main>
+
+  </div>
 );
 
 export default Home;
