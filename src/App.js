164b54b688614f97cb07893672e9e1098a57b8ea
Lembani <Lembanisakala@gmail.com>
2022-09-28 21:40:43 +0200

    Update App with changes from dev

--- src/App.js
+++ src/App.js
@@ -12,48 +12,56 @@ import Categories from './Components/categories/Index';
 import Category from './Components/categories/Show';
 import store from './Redux/store';
 import { FormProvider } from './Context/FormContext';
+import { MenuProvider } from './Context/MenuContext';
 import AddHotel from './Components/Hotels/AddHotel';
-import Hotels from './Components/Hotels/Hotels';
+import HotelsComponent from './Components/Hotels/Hotels';
 import HotelDetails from './Components/Hotels/HotelDetails';
 import DeleteHotel from './Components/Hotels/DeleteHotel';
 import Login from './Components/Auth/Login';
 import Register from './Components/Auth/Register';
+import Reservations from './Components/Reservations';
+import CreateReservation from './Components/CreateReservation';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route element={<Private />}>
-            <Route exact path="/hotels" element={<Hotels />} />
-            <Route
-              exact
-              path="/categories"
-              element={
-                <FormProvider>
-                  <Categories />
-                </FormProvider>
+      <MenuProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route element={<Private />}>
+              <Route exact path="/hotels" element={<HotelsComponent />} />
+              <Route
+                exact
+                path="/categories"
+                element={
+                  <FormProvider>
+                    <Categories />
+                  </FormProvider>
                     }
-            />
-            <Route
-              exact
-              path="/categories/:id"
-              element={
-                <FormProvider>
-                  <Category />
-                </FormProvider>
+              />
+              <Route
+                exact
+                path="/categories/:id"
+                element={
+                  <FormProvider>
+                    <Category />
+                  </FormProvider>
                       }
-            />
-            <Route exact path="/hotels" element={<HotelDetails />}>
-              <Route path=":id" element={<HotelDetails />} />
+              />
+              <Route exact path="/hotels" element={<HotelDetails />}>
+                <Route path=":id" element={<HotelDetails />} />
+              </Route>
+              <Route exact path="/add_hotel" element={<AddHotel />} />
+              <Route exact path="/delete_hotel" element={<DeleteHotel />} />
+              <Route exact path="/reservations" element={<Reservations />} />
+              <Route exact path="/add_reservation" element={<CreateReservation />} />
             </Route>
-            <Route exact path="/add_hotel" element={<AddHotel />} /><Route exact path="/delete_hotel" element={<DeleteHotel />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </MenuProvider>
     </Provider>
   );
 }
