980406f5ed021c4af8f4f32c8d9280e83512c2e6
Lembani <Lembanisakala@gmail.com>
2022-09-29 16:55:53 +0200

    Uodate Register styles

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -36,38 +36,47 @@ const Register = () => {
   };
 
   return (
-    <div className="container d-md-flex">
-      <section className="section-form d-flex flex-column align-items-center mt-4 mt-md-5 pt-md-3">
-        <div className="form-title">
-          <p className="h3">Register For an Account</p>
-        </div>
+    <section className="section-form">
+      <h2 className="form-title">Register For An Account</h2>
 
-        <div className="form-container">
+      <div className="form-container">
+        <div className="register-form-container">
           <form className="register-form" onSubmit={handleSubmit}>
             <div className="form-group">
-              <p className="ms-4 mb-1">Name</p>
-              <input type="text" name="name" id="register-name" className="form-control rounded-pill" placeholder="Enter your name" required onChange={(e) => setName(e.target.value)} />
+              <label htmlFor="name">
+                Name
+                <br />
+                <input type="text" name="name" id="register-name" className="form-control" placeholder="Enter your name" required onChange={(e) => setName(e.target.value)} />
+              </label>
             </div>
             <div className="form-group">
-              <p className="ms-4 mb-1">Email</p>
-              <input type="text" name="email" id="register-email" className="form-control rounded-pill" placeholder="Enter your email" required onChange={(e) => setEmail(e.target.value)} />
+              <label htmlFor="email">
+                Email
+                <br />
+                <input type="text" name="email" id="register-email" className="form-control" placeholder="Enter your email" required onChange={(e) => setEmail(e.target.value)} />
+              </label>
             </div>
             <div className="form-group">
-              <p className="ms-4 mb-1">Password</p>
-              <input type="password" name="password" id="register-password" className="form-control rounded-pill" placeholder="Enter your password" required onChange={(e) => setPassword(e.target.value)} />
+              <label htmlFor="password">
+                Password
+                <br />
+                <input type="password" name="password" id="register-password" className="form-control" placeholder="Enter your password" required onChange={(e) => setPassword(e.target.value)} />
+              </label>
             </div>
 
             <div className="form-group">
-              <p className="ms-4 mb-1">Confirm Password</p>
-              <input type="password" name="password_confirmation" id="confirm_register_password" className="form-control rounded-pill" placeholder="Confirm password" required onChange={(e) => setPasswordConfirmation(e.target.value)} />
+              <label htmlFor="password_confirmation">
+                Confirm Password
+                <br />
+                <input type="password" name="password_confirmation" id="confirm_register_password" className="form-control" placeholder="Confirm password" required onChange={(e) => setPasswordConfirmation(e.target.value)} />
+              </label>
             </div>
 
-            <div className="register-buttons mt-4 text-center">
-              <button type="submit" className="style-btn rounded-pill" to="/signin">Register</button>
+            <div className="register-buttons">
+              <button type="submit" className="register-btn" to="/signin">Register</button>
             </div>
           </form>
-
-          <div className="notice-account mt-3">
+          <div className="notice-account">
             <p>
               Already have an account?
               {' '}
@@ -75,8 +84,8 @@ const Register = () => {
             </p>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
